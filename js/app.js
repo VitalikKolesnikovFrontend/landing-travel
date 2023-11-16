421fe7a1518cd64339b465fa1537f8928acc7533
Vitalik Kolesnikov <kolesnikov.frontend@gmail.com>
2023-11-16 18:57:29 +0300

    test

--- js/app.js
+++ js/app.js
@@ -2,7 +2,12 @@ document.addEventListener('DOMContentLoaded', function(event) {
   const modal = document.querySelector('.modal')
   const modalBtn = document.querySelector('.business__icon')
   const closeBtn = document.querySelector('.modal__button')
+  const registrationTitle = document.querySelector('.registration__title')
+  const registrationModal = document.querySelector('.registration__modal')
 
+  registrationTitle.addEventListener('click', () => {
+    registrationModal.classList.toggle('registration__close')
+  })
 
   modalBtn.addEventListener('click', () => {
     modal.classList.remove('modal');
